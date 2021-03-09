cc047433025048c537437862aa0fcb22be24f9e8
Audu Billy <audubilly89@gmail.com>
2021-03-09 17:16:01 +0100

    updates

--- SandwichCalculator.js
+++ SandwichCalculator.js
@@ -7,5 +7,13 @@ function sandwichCalculator(slicesOfBread){
 console.log(sandwichCalculator(11));
 
 function sandwichCalculator2(slicesOfBread,slicesOfCheese){
-    if(slicesOfBread / 2)
-}
+    
+      if  (sandwichCalculator(slicesOfBread) >= slicesOfCheese){
+          return slicesOfCheese;
+      }else if(sandwichCalculator(slicesOfBread) < slicesOfCheese){
+          return sandwichCalculator(slicesOfBread);
+      }
+
+      }
+    
+     console.log(sandwichCalculator2(6,10));
